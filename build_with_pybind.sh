79e74808ed5e862301b34ffd5344b8fcc4eebdaf
Monster Cookie <13055407031@163.com>
2024-10-25 16:04:30 +0800

    脚步文件补充提交

---
+++ build_with_pybind.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 设置 build 目录路径
+buildDir="native_cpp/build"
+
+# 检查目录是否存在，如果存在则删除
+if [ -d "$buildDir" ]; then
+    rm -rf "$buildDir"
+fi
+
+# 创建新的 build 目录
+mkdir "$buildDir"
+
+# 进入 build 目录
+cd "$buildDir" || exit
+
+# 执行 cmake ..
+cmake -DENABLE_PYBIND=ON ..
+
+# 执行编译
+cmake --build .
+
+# 设置目标目录路径
+sourceDir="./"
+libsDir="../../libs"
+
+# 确保目标 libs 目录存在
+if [ ! -d "$libsDir" ]; then
+    mkdir -p "$libsDir"
+fi
+
+# 复制 .so 文件到 libs 目录
+for file in "$sourceDir"/*.so; do
+    [ -e "$file" ] || continue  # 跳过不存在的文件
+    cp -f "$file" "$libsDir"
+done
+
---
+++ 双击构建_Pybind.ps1
@@ -0,0 +1,51 @@
+# 设置 build 目录路径
+$buildDir = "native_cpp/build"
+
+# 检查目录是否存在，如果存在则删除
+if (Test-Path $buildDir) {
+    Remove-Item -Recurse -Force $buildDir
+}
+
+# 创建新的 build 目录
+New-Item -ItemType Directory -Path $buildDir
+
+# 进入 build 目录
+Set-Location $buildDir
+
+# 执行 cmake ..
+cmake -DENABLE_PYBIND=ON ..
+
+# 执行编译
+cmake --build .
+
+# 设置目标目录路径
+$debugDir = "Debug"
+$releaseDir = "Release"
+$libsDir = "../../libs"
+
+# 检查 Debug 目录是否存在
+if (Test-Path $debugDir) {
+    $sourceDir = $debugDir
+} elseif (Test-Path $releaseDir) {
+    $sourceDir = $releaseDir
+} else {
+    Write-Host "Neither Debug nor Release directory found."
+    Write-Host "Press any key to exit..."
+    $x = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    exit
+}
+
+# 确保目标 libs 目录存在
+if (-Not (Test-Path $libsDir)) {
+    New-Item -ItemType Directory -Path $libsDir
+}
+
+# 复制 .pyd 文件到 libs 目录
+$files = Get-ChildItem -Path "$sourceDir\*.pyd"
+foreach ($file in $files) {
+    Copy-Item -Path $file.FullName -Destination $libsDir -Force
+}
+
+# 等待按键，以阻止窗口关闭
+Write-Host "Press any key to exit..."
+$x = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
